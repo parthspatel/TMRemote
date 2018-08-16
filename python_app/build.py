3c8f669f070003df7822cb3a0b1aacfbb317a8d2
Parth Patel <parthspatel.nj@gmail.com>
2018-08-16 10:49:56 -0400

    fixed syntax error

--- python_app/build.py
+++ python_app/build.py
@@ -1,6 +1,6 @@
 import os
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     value = os.system('python setup.py build')
     if value == 1:
         value = os.system('python setup.py build')
