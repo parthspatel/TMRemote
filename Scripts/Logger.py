7d05003ead444560321e43397c2c32c91938129c
Mehodin <32092714+Mehodin@users.noreply.github.com>
2018-08-17 13:31:36 +0200

    added readme

--- Scripts/Logger.py
+++ Scripts/Logger.py
@@ -1,2 +1,5 @@
-import TMRLogger
-TMRLogger.Log().main()
+try:
+	import TMRLogger
+	TMRLogger.Log().main()
+except ModuleNotFoundError:
+	print('> Could not find TMRLogger inside Appdata folder, please place it there and restart client.')
