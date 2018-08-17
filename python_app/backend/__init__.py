690d1972d5ba18dab4684f154c03ecf64c123e61
Parth Patel <parthspatel.nj@gmail.com>
2018-08-17 14:50:30 -0400

    added client launcher to backend

--- python_app/backend/__init__.py
+++ python_app/backend/__init__.py
@@ -1,7 +1,7 @@
 from .auth import Auth
 from .banDetection import BanDetectionThread
 from .botLogging import BotLoggingThread
-# from .clientLauncher import ClientLauncher
+from .clientLauncher import ClientLauncher
 from .log import LogThread, Log
 from .mainThread import MainThread
 from .nexonServer import NexonServer
