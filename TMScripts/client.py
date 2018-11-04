3364888f83bb6560d0309a5c2cd6874330b366b9
Parth Patel <parthspatel.nj@gmail.com>
2018-11-04 01:35:01 -0500

    update working mutliclient server

--- TMScripts/client.py
+++ TMScripts/client.py
@@ -1,11 +1,30 @@
 import socket
+import sys
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+def main():
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = "127.0.0.1"
+    port = 8888
 
-print('Received', repr(data))
+    try:
+        soc.connect((host, port))
+    except:
+        print("Connection error")
+        sys.exit()
+
+    print("Enter 'quit' to exit")
+    message = input(" -> ")
+
+    while message != 'quit':
+        soc.sendall(message.encode("utf8"))
+        if soc.recv(5120).decode("utf8") == "-":
+            pass        # null operation
+
+        message = input(" -> ")
+
+    soc.send(b'--quit--')
+
+
+if __name__ == "__main__":
+    main()
